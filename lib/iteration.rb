f06adecc8b8c10e377d32372dd03c758a8622063
Cameron Rhinehardt <rhinehardtc@gmail.com>
2020-03-29 01:13:52 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -42,7 +42,7 @@ def total_even_pairs(src)
   d = 0
   a = 0
   while d < src.length do 
-      new_array.push(d[a] + (d[a + 1]))
+    new_array.push(d[a] + (d[a + 1])) if d[a] % 2 = 0 && d[a + 1] % 2 = 0
     d += 1
   end
   new_array
